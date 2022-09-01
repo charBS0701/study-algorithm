a8d1bf19372329dee2ba18c005bc1443a347ccd2
charbs0701 <chaqhdtjr7@gmail.com>
2022-09-01 22:20:08 +0900

    구현기출 3

---
+++ python-for-coding-test/12-3_문자열압축.py
@@ -0,0 +1,11 @@
+def solution(s):
+    for i in range(len(s)//2):
+        for j in range(s):
+            
+            n_s = s[j:j+i]
+
+data = input() # 문자열 입력
+
+print(solution(data))
+
+# 모르겠다 ~
