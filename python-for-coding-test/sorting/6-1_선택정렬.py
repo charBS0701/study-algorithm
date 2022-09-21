d77b467dae348c45ea0875b910bfb5142b49f87a
charbs0701 <chaqhdtjr7@gmail.com>
2022-09-22 03:40:33 +0900

    정렬 개념, 선택정렬, 삽입정렬

---
+++ python-for-coding-test/sorting/6-1_선택정렬.py
@@ -0,0 +1,17 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i # 가장 작은 원소의 인덱스
+    for j in range(i+1,len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i] # 스와프
+
+    # min_index = array.index(min(array[i:]))
+    # temp = array[i]
+    # array[i] = array[min_index]
+    # array[min_index] = temp
+
+print(array)
+
+    
