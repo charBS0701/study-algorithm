66552963e2ebb6837be81d1a76e76ee6185ee930
charbs0701 <chaqhdtjr7@gmail.com>
2023-02-11 13:50:11 +0900

    완주하지못한자 sol추가

---
+++ programmers/위장.py
@@ -0,0 +1,11 @@
+# 위장
+# https://campus.programmers.co.kr/tryouts/63117/challenges?language=python3
+# [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]	5
+
+def solution(clothes):
+    answer = 0
+    
+
+    return answer
+
+print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
